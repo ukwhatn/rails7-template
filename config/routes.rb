3e41026973b824a4b0e73570febd777c863efb14
Yuki Watanabe <ukwhatn@gmail.com>
2023-09-04 04:43:39 +0900

    remote test routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'test#index'
-  get 'test/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
